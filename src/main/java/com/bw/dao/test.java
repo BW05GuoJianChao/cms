e63697575c04cdc05695d986916f3b6f8126f068
BW05GuoJianChao <2605579851@qq.com>
2019-09-05 15:04:03 +0800

    第一次测试版本

---
+++ src/main/java/com/bw/dao/test.java
@@ -0,0 +1,15 @@
+package com.bw.dao;
+/**
+ * @作者 国建超
+ * @日期 2019年9月5日
+ * @时间 下午2:57:23
+ * 类功能：
+ *
+ */
+public class test {
+		
+	 public static void main(String[] args) {
+		System.out.println("hellow");
+	}
+	
+}
